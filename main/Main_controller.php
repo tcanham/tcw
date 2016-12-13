69adc1d7a671866d32ffafde9aa8875466fd1aae
tcanham <tascanham@gmail.com>
2016-12-13 19:52:27 +0000

    initial commit

---
+++ main/Main_controller.php
@@ -0,0 +1,12 @@
+<?php
+/*
+* This is the main controller, it contains all the functions to be utilised by other controllers.
+*
+*/
+
+Class Main_controller{
+  
+    public function _contruct(){
+      
+    }
+}
